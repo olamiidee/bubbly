9b17639e5bbf336f5030b79ddb87232c05bec2a5
olamiidee <osibeluwoolamide@gmail.com>
2022-06-15 16:30:51 +0100

    commit

--- script.js
+++ script.js
@@ -24,3 +24,52 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
+var myIndex = 0;
+carousel();
+var slides, dots;
+
+function carousel() {
+  var i;
+   slides = document.getElementsByClassName("mySlides");
+   dots = document.getElementsByClassName("dot");
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none"; 
+    }
+    myIndex++;
+    if (myIndex > slides.length) {myIndex = 1}   
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    } 
+    slides[myIndex-1].style.display = "block"; 
+    dots[myIndex-1].className += " active"; 
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+function plusSlides(position) {
+    myIndex +=position;
+    if (myIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+function currentSlide(index) {
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index-1].style.display = "block";  
+    dots[index-1].className += " active";
+}
